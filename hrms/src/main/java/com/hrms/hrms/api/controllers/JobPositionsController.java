f2c5b4285848236be72ae69b0c6083002a9d6be9
Tülin Aktaş <tulin-aktas@hotmail.com>
2021-06-10 23:03:48 +0300

    swagger

--- hrms/src/main/java/com/hrms/hrms/api/controllers/JobPositionsController.java
+++ hrms/src/main/java/com/hrms/hrms/api/controllers/JobPositionsController.java
@@ -3,6 +3,7 @@ package com.hrms.hrms.api.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import com.hrms.hrms.business.abstracts.JobPositionService;
 import com.hrms.hrms.entities.concretes.JobPosition;
 
 @RestController
-@RequestMapping("/api/job_positions")
+@RequestMapping("/api/jobpositions")
 public class JobPositionsController {
 	
 	private JobPositionService jobPositionService;
@@ -21,7 +22,6 @@ public class JobPositionsController {
 		super();
 		this.jobPositionService = jobPositionService;
 	}
-
 
 	@GetMapping("/getall")
 	public List<JobPosition> getAll(){
